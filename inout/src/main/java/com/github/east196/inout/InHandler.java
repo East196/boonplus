2fce6825be5f34a4376db7e5a1cf54ad24bc3480
east196 <2901180515@qq.com>
2017-08-04 14:21:02 +0800

    delete inout

--- inout/src/main/java/com/github/east196/inout/InHandler.java
+++
@@ -1,7 +0,0 @@
-package com.github.east196.inout;
-
-public class InHandler {
-    public Out handle(In in) {
-        return null;
-    }
-}
--- inout/src/main/java/com/github/east196/inout/Out.java
+++
@@ -1,4 +0,0 @@
-package com.github.east196.inout;
-
-public class Out {
-}
